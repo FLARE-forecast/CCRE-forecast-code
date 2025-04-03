95d58f26332b4e83309114d0f566d9180ed8ff62
addelany <addelany@gmail.com>
2025-04-03 16:48:10 -0500

    fix v3 workflow

--- workflows/glm_flare_v3/combined_forecast.R
+++ workflows/glm_flare_v3/combined_forecast.R
@@ -2,7 +2,8 @@ library(tidyverse)
 library(lubridate)
 
 #remotes::install_github('flare-forecast/FLAREr@single-parameter')
-remotes::install_github("cboettig/aws.s3")
+#remotes::install_github("cboettig/aws.s3")
+
 Sys.setenv('GLM_PATH'='GLM3r')
 
 lake_directory <- here::here()
@@ -18,75 +19,20 @@ Sys.setenv("AWS_DEFAULT_REGION" = "renc",
            "AWS_S3_ENDPOINT" = "osn.xsede.org",
            "USE_HTTPS" = TRUE)
 
+config_obs <- yaml::read_yaml(file.path(lake_directory,'configuration',config_set_name,'observation_processing.yml'))
+configure_run_file <- "configure_run.yml"
+config <- FLAREr:::set_up_simulation(configure_run_file,lake_directory, config_set_name = config_set_name)
+
 #FLAREr:::ignore_sigpipe()
 
 noaa_ready <- TRUE
 
+message("Beginning generate targets")
+source('./workflows/glm_flare_v3/generate_targets.R')
 
 while(noaa_ready){
   
-  message("Beginning generate targets")
-  
-  #' Generate the `config_obs` object and create directories if necessary
-  
-  #config_obs <- FLAREr:::initialize_obs_processing(lake_directory, observation_yml = "observation_processing.yml", config_set_name = config_set_name)
-  
-  config_obs <- yaml::read_yaml(file.path(lake_directory,'configuration',config_set_name,'observation_processing.yml'))
-  
-  configure_run_file <- "configure_run.yml"
   config <- FLAREr:::set_up_simulation(configure_run_file,lake_directory, config_set_name = config_set_name)
-  
-  
-  dir.create(file.path(lake_directory, "targets", config$location$site_id), showWarnings = FALSE)
-  #' Clone or pull from data repositories
-  
-  FLAREr:::get_git_repo(lake_directory,
-                       # directory = config_obs$insitu_obs_fname,
-                       directory = config_obs$realtime_temp_location,
-                       # directory = config_obs$realtime_insitu_location,
-                       git_repo = "https://github.com/FLARE-forecast/CCRE-data.git"
-  )
-  
-  #' Download files from EDI
-  
-  get_edi_file(#edi_https = "https://pasta.lternet.edu/package/data/eml/edi/1069/1/57267535da5ab0687d2fee52083699f8", #calwalk EDI ##update this url when data is published 
-    edi_https = 'https://portal-s.edirepository.org/nis/dataviewer?packageid=edi.719.20&entityid=2ecdcd6114591d6a798ecce9050c13c7',
-    file = config_obs$insitu_obs_fname[2],
-    lake_directory)
-  
-  get_edi_file(#edi_https = "https://pasta.lternet.edu/package/data/eml/edi/1069/1/b391093432e38eee7c7cc34ae977d553", #depth offset EDI ##update this url when data is published 
-    edi_https = 'https://portal-s.edirepository.org/nis/dataviewer?packageid=edi.719.20&entityid=da210012e686ffbde699fb6e49cb0c9c',
-    file = config_obs$insitu_obs_fname[3],
-    lake_directory)
-  
-  
-  #' Clean up observed insitu measurements
-  cleaned_insitu_file <- in_situ_qaqc(insitu_obs_fname = file.path(lake_directory,"data_raw", config_obs$insitu_obs_fname),
-                                      data_location = file.path(lake_directory,"data_raw"),
-                                      maintenance_file = file.path(lake_directory, "data_raw", config_obs$maintenance_file),
-                                      ctd_fname = NA,
-                                      nutrients_fname =  NA,
-                                      secchi_fname = NA,
-                                      cleaned_insitu_file = file.path(lake_directory,"targets", 
-                                                                      config$location$site_id,
-                                                                      paste0(config$location$site_id,"-targets-insitu.csv")),
-                                      site_id = config$location$site_id,
-                                      config = config_obs)
-  
-  #' Move targets to s3 bucket
-  
-  message("Successfully generated targets")
-  
-  FLAREr:::put_targets(site_id = config_obs$lake_name_code,
-                      cleaned_insitu_file,
-                      cleaned_met_file = NA,
-                      cleaned_inflow_file = NA,
-                      use_s3 = config$run_config$use_s3,
-                      config = config)
-  
-  message("Successfully moved targets to s3 bucket")
-  
-  
   
   # Run FLARE
   message('Running forecast...')
